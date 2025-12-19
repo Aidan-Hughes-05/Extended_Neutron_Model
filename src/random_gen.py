32898205ac32dd572c53693321fa78ded583224d
aidanhughes <a.jameshughes11@gmail.com>
2025-12-19 13:30:14 +0000

    Add random_gen.py, .gitignore, requirements.txt and added isotropic unit vector generator to random_gen.py

---
+++ src/random_gen.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def exp_rand(rate_param, sample_size = 1):
+    rand_uniform = np.random.uniform(size = sample_size) # generates uniform rand number set
+    return -rate_param * np.log(rand_uniform) # acts inverse CDF to get exponential distribution
+
+def isotropic_unit_vector(sample_size = 1):
+    phi = np.random.uniform(low = -np.pi, high = np.pi, size=sample_size) # produced random uniform values -pi to pi
+    theta = np.arccos(1-2*np.random.uniform(size=sample_size)) # uses inverse CDF to produce theta values that wont pole
+    # convert to cartesian co ordinates
+    sin_theta = np.sin(theta)
+    x = sin_theta*np.cos(phi)
+    y = sin_theta*np.sin(phi)
+    z = np.cos(theta)
+    return np.array([x, y, z]) # output as 2D array
